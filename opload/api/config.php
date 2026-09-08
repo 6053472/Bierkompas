12783a44ab61104174b9cd097bddef02f84864ba
MaysonRoeleveld <102528@glr.nl>
2026-09-08 11:11:32 +0000

    backend upload update

---
+++ opload/api/config.php
@@ -0,0 +1,8 @@
+<?php
+// Kopieer dit bestand naar "config.php" en vul de gegevens in die je van je hosting/phpMyAdmin krijgt.
+// config.php zelf staat in .gitignore, zodat je wachtwoord nooit in git terechtkomt.
+
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'bierkomp2_database');
+define('DB_USER', 'bierkomp2_user');
+define('DB_PASS', 'Welkom1234#');
